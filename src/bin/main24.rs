f4c1f922a30d270726ba6ac63e7021f8014a5466
yasuabe2613 <yasu@yasuabe.net>
2025-04-07 03:15:47 +0900

    solve Q24

---
+++ src/bin/main24.rs
@@ -0,0 +1,33 @@
+use exercises_for_programmer::utils::std_util::read_input;
+
+struct Input { first: String, second: String }
+
+impl Input {
+    fn inputs(&self) -> (&str, &str) { (&self.first, &self.second) }
+}
+fn read() -> Input {
+    println!("Enter two strings and I'll tell you if they are anagrams:");
+    Input {
+        first:  read_input("Enter the first string: "),
+        second: read_input("Enter the second string: ")
+    }
+}
+fn sorted(s: &str) -> String {
+    let mut result = s.chars().collect::<Vec<char>>();
+    result.sort_unstable();
+    result.iter().collect()
+}
+fn is_anagram(first: &str, second: &str) -> bool {
+    first.len() == second.len() // redundant, but the constraints of Q24
+      && sorted(first) == sorted(second)
+}
+fn print_output(input: Input) {
+    let (f, s)     = input.inputs();
+    let conclusion = if is_anagram(f, s) { "" } else { "not " };
+
+    println!("\"{}\" and \"{}\" are {}anagrams.", f, s, conclusion)
+}
+fn main() {
+    let input  = read();
+    print_output(input);
+}
