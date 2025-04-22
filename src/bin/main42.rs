39b40c5a51aa69e1c9a7ad9d6ee70657d510762a
yasuabe2613 <yasu@yasuabe.net>
2025-04-23 00:41:12 +0900

    solve ex42

--- src/bin/main42.rs
+++ src/bin/main42.rs
@@ -22,8 +22,16 @@ struct Employee {
     last_name:  String,
     salary:     u32,
 }
-
-fn mk_employee<'a>(caps: Captures<'a>) -> Result<Employee> {
+fn num_digits(mut n: u32) -> usize {
+    if n == 0 { return 1; }
+    let mut len = 0;
+    while n > 0 {
+        len += 1;
+        n /= 10;
+    }
+    len
+}
+fn employee_from_caps<'a>(caps: Captures<'a>) -> Result<Employee> {
     caps["salary"]
         .parse()
         .map(|salary| {
@@ -33,48 +41,54 @@ fn mk_employee<'a>(caps: Captures<'a>) -> Result<Employee> {
                 salary,
             }
         })
-        .with_context(|| format!( "Failed to parse salary from") )  
+        .with_context(|| format!( "Failed to parse salary field") )  
 }
-fn deserialize_emloyee(line: &str) -> Result<Employee> {
+fn employee_from_line(line: &str) -> Result<Employee> {
     match CSV_REGEX.captures(line) {
-        Some(caps) => mk_employee(caps),
-        None       => Err(anyhow::anyhow!("Failed to parse line"))
+        Some(caps) => employee_from_caps(caps),
+        None       => Err(anyhow::anyhow!("Line does not match expected CSV format"))
     }
 }
-fn load_csv() -> Result<String> {
+fn read_csv_file() -> Result<String> {
     std::fs::read_to_string(INPUT_PATH)
         .with_context(|| format!("Failed to load {}", INPUT_PATH))
 }
-fn parse_csv(raw_csv: &str) -> Result<((usize, usize, usize), Vec<Employee>)> {
+fn parse_all_employees(raw_csv: &str) -> Result<Vec<Employee>> {
     let mut employees = Vec::new();
-    let (mut w1, mut w2, mut w3) = (0, 0, 0);
-    let mut f = |e: &Employee| {
-        w1 = max(w1, e.first_name.len());
-        w2 = max(w2, e.last_name.len());
-        w3 = max(w3, e.salary.to_string().len());
-        employees.push(e.clone());
-    };
     for (idx, line) in raw_csv.lines().enumerate() {
         if line.is_empty() { continue; }
-        match deserialize_emloyee(line) {
-            Ok(employee) => f(&employee),
-            Err(e)       => eprintln!("Failed to parse line {}: {}", idx, e),
+        match employee_from_line(line) {
+            Ok(employee) => employees.push(employee),
+            Err(e)       => eprintln!("Error parsing line {} ({}): {}", idx + 1, line, e),
         }
     }
-    Ok(((w1, w2, w2), employees.clone()))
+    Ok(employees)
 }
-fn display_employees((w1, w2, w3): (usize, usize, usize), employees: &[Employee]) -> Result<()> {
+fn calculate_col_widths(employees: &[Employee]) -> (usize, usize, usize) {
+    employees.iter().fold((0, 0, 0), |(w1, w2, w3), e| {(
+        max(w1, e.first_name.len()),
+        max(w2, e.last_name.len()),
+        max(w3, num_digits(e.salary))
+    )})
+}
+fn print_header((w1, w2, w3): (usize, usize, usize)) {
     println!("{:^w1$} {:^w2$} {:^w3$}", "First", "Last", "Salary");
     println!("{}", "-".repeat(w1 + w2 + w3 + 2));
-    for employee in employees {
-        println!("{:<w1$} {:<w2$} {:>w3$}", employee.first_name, employee.last_name, employee.salary);
-    }
-    Ok(())
+}
+fn print_employee_row((w1, w2, w3): (usize, usize, usize), e: &Employee) {
+    println!("{:<w1$} {:<w2$} {:>w3$}", e.first_name, e.last_name, e.salary);
+}
+fn print_employee_table(employees: &[Employee]) {
+    let col_widths = calculate_col_widths(&employees);
+
+    print_header(col_widths);
+    employees.iter().for_each(|e| print_employee_row(col_widths, e))
 }
 fn main() -> Result<()> {
-    let raw_csv   = load_csv()?;
-    let (max_widths, employees) = parse_csv(&raw_csv)?;
-    display_employees(max_widths, &employees)?;
+    let raw_csv   = read_csv_file()?;
+    let employees = parse_all_employees(&raw_csv)?;
+
+    print_employee_table(&employees);
     Ok(())
 }
 
@@ -82,8 +96,8 @@ fn main() -> Result<()> {
 mod tests {
     use super::*;
 
-    fn assert_deserialization(line: &str, last: &str, first: &str, salary: u32) {
-        let employee = deserialize_emloyee(line);
+    fn assert_success(line: &str, last: &str, first: &str, salary: u32) {
+        let employee = employee_from_line(line);
         assert_eq!(employee.is_ok(), true, "failed to deserialize at line: {}", line);
 
         let employee = employee.unwrap();
@@ -91,21 +105,22 @@ mod tests {
         assert_eq!(employee.last_name,  last  , "{}", line);
         assert_eq!(employee.salary,     salary, "{}", line);
     }
-    fn assert_deserializatino_failure(line: &str) {
-        assert_eq!(deserialize_emloyee(line).is_ok(), false, "at line: {}", line);
+    fn assert_parse_failure(line: &str) {
+        assert_eq!(employee_from_line(line).is_ok(), false, "at line: {}", line);
     }
     #[test]
-    fn test_deserialization_success2() {
-        assert_deserialization("Jones,Aaron,46000" , "Jones", "Aaron", 46000);
-        assert_deserialization(" Ling, Mai , 5590" , "Ling" , "Mai"  ,  5590);
-        assert_deserialization(" a  a , b b , 55  ", "a  a" , "b b"  ,    55);
+    fn test_parse_success() {
+        assert_success("Jones,Aaron,46000" , "Jones", "Aaron", 46000);
+        assert_success(" Ling, Mai , 5590" , "Ling" , "Mai"  ,  5590);
+        assert_success(" a  a , b b , 55  ", "a  a" , "b b"  ,    55);
     }
 
-    fn test_deserialization_failure() {
-        assert_deserializatino_failure("");
-        assert_deserializatino_failure("a");
-        assert_deserializatino_failure("a,b");
-        assert_deserializatino_failure("a,b,c");
-        assert_deserializatino_failure("a,b,-1");
+    #[test]
+    fn test_parse_failure() {
+        assert_parse_failure("");
+        assert_parse_failure("a");
+        assert_parse_failure("a,b");
+        assert_parse_failure("a,b,c");
+        assert_parse_failure("a,b,-1");
     }
 }
